124458b4b5132c78445012085f30c2a38f75d69b
ordinemickael <ordinemickael99@gmail.com>
2025-10-14 13:14:58 +0200

    added a new class for StlList

--- Container/Deque.h
+++ Container/Deque.h
@@ -1,4 +1,7 @@
 ﻿#pragma once
+#include <cstddef>
+#include <stdexcept>
+#include <iostream>
 
 /*
  *  Deque template class
@@ -10,28 +13,95 @@
  *  the map is a contiguis array of pointer to chunks
  */
 
-#define DEFAULT_DEQUE_SIZE 8
+#define DEFAULT_MAP_SIZE    8
 #define DEFAULT_CHUNK_SIZE 20
 
 
 template<typename T>
 class Deque<T>
-{
-    // MEMBERS VARIABLE //
-public:
+    {
+        // MEMBERS VARIABLE //
+    public:
+    
 
-protected:
+    protected:
 
-private:
+    private:
+        T** map;              // map of pointers to chunks
+        size_t map_size;      // size of map
+        size_t first_block;   // first block index of map
+        size_t last_block;    // last block index of map
+        size_t first_elem;    // index of the first elem
+        size_t last_elem;     // index of the last elem
+        size_t count;         // count of elements in the Deque
+        
+        // MEMBERS VARIABLE //
 
-    // MEMBERS VARIABLE //
+        // MEMBERS FUNCTIONS //
+    public:
+    Deque() : map(nullptr), map_size(0), first_block(0), last_block(0), first_elem(0), last_elem(0), count(0)
+    {
+        // create a Deque with default map size and chunk size
+        map_size = DEFAULT_MAP_SIZE;
+        map = new T * [map_size];
+        for (size_t i = 0; i < map_size; i++)
+        {
+            map[i] = nullptr;
+        }
+        first_block = map_size / 2;
+        last_block = first_block;
+        map[first_block] = new T[DEFAULT_CHUNK_SIZE];
+        first_elem = DEFAULT_CHUNK_SIZE / 2; // starting from the middle of the chunk
+        last_elem = first_elem;
+    }                     // default constructor
+    Deque(const Deque &other) 
+    {
+        map_size = other.map_size;
+        map = new T * [map_size];
+        for (size_t i = 0; i < map_size; i++)
+        {
+            if (other.map[i] != nullptr)
+            {
+                map[i] = new T[DEFAULT_CHUNK_SIZE];
+                for (size_t j = 0; j < DEFAULT_CHUNK_SIZE; j++)
+                {
+                    map[i][j] = other.map[i][j];
+                }
+            }
+            else
+            {
+                map[i] = nullptr;
+            }
+        }
+        first_block = other.first_block;
+        last_block = other.last_block;
+        first_elem = other.first_elem;
+        last_elem = other.last_elem;
+        count = other.count;
+    }   // copy constructor
+    ~Deque()
+    {
+        // Dealloca solo i blocchi effettivamente allocati
+        for (size_t i = first_block; i <= last_block && map != nullptr; i++)
+        {
+            if (map[i] != nullptr)
+            {
+                delete[] map[i];
+            }
+                
+        }
+    
+        // Poi dealloca la map
+        delete[] map;
+    } 
+    
+    
+    protected:
 
-    // MEMBERS FUNCTIONS //
-public:
+    private:
 
-protected:
+        // MEMBERS FUNCTIONS //
+    };
 
-private:
 
-    // MEMBERS FUNCTIONS //
-};
+
---
+++ Container/List.h
@@ -0,0 +1,168 @@
+﻿#include <iostream>
+#include <ostream>
+
+namespace MyStl
+{
+    template<typename T>
+    struct Node
+    {
+        T data;
+        Node<T>* next;
+        Node<T>* prev;
+
+        Node() : data(), next(nullptr), prev(nullptr) {} // default constructor
+        ~Node() = default;
+        Node(const T& value) : data(value), next(nullptr), prev(nullptr) {} // copy constructor
+        Node(T&& value) : data(std::move(value)), next(nullptr), prev(nullptr) {} // Move constructor
+        
+    };
+    
+    template<typename T>
+    class List
+    {
+        // MEMBERS VARIABLES //
+    public:
+        
+    protected:
+        
+    private:
+        Node<T>* m_head;
+        Node<T>* m_tail;
+        size_t m_size;
+        // MEMBERS VARIABLES //
+
+        // MEMBERS FUNCTIONS //
+    public:
+        // ===== CONSTRUCTORS =====
+        List() : m_head(nullptr), m_tail(nullptr), m_size(0){ std::cout << "List constructor called " << std::endl;} // default constructor
+        List(const List& other) : m_head(nullptr), m_tail(nullptr), m_size(0)
+        {
+            Node<T>* temp = other.m_head;
+            while (temp != nullptr)
+            {
+                // we copy all the element inside the new list
+                push_back(temp->data);
+                temp = temp->next;
+            }
+        } // copy constructor
+        List(const List&& other) noexcept : m_head(other.m_head), m_tail(other.m_tail), m_size(other.Size())
+        {
+            other.m_head = nullptr;
+            other.m_tail = nullptr;
+            other.m_size = 0;
+        } // move construtor
+        List& operator=(const List& other)
+        {
+            if (this != &other)
+            {
+                clear();
+                Node<T>* temp = other.m_head;
+                while (temp != nullptr)
+                {
+                    // we copy all the element inside the new list
+                    push_back(temp->data);
+                    temp = temp->next;
+                }
+            }
+            return *this;
+        } // copy assignment
+        List& operator=(List&& other) noexcept
+        {
+            if (this != &other)
+            {
+                clear();
+                m_head = other.m_head;
+                m_tail = other.m_tail;
+                m_size = other.m_size;
+                other.m_head = nullptr;
+                other.m_tail = nullptr;
+                other.m_size = 0;
+            }
+            return *this;
+        } // move assignment
+        ~List()
+        {
+            clear();
+            std::cout << "List destructor called" << std::endl;
+        }
+
+        // ===== GETTER =====
+        inline Node<T>* Head(){ return m_head; }
+        inline Node<T>* Tail() { return m_tail; }
+
+        // ===== CAPACITY =====
+        inline size_t Size() const { return m_size; }
+        inline bool Empty() const { return m_size == 0; }
+
+        // ===== ELEMENT ACCESS =====
+        T& front()
+        {
+            
+        }
+        const T& front() const
+        {
+            
+        }
+        T& back()
+        {
+            
+        }
+        const T& back() const
+        {
+            
+        }
+
+        // ===== ELEMENT MODIFIERS =====
+        void push_back(const T& value)
+        {
+            
+        }
+        void push_front(const T& value)
+        {
+            
+        }
+        void push_back(const T&& value)
+        {
+            
+        }
+        void push_front(const T&& value)
+        {
+            
+        }
+        void pop_front()
+        {
+            
+        }
+        void pop_back()
+        {
+            
+        }
+        void clear()
+        {
+            while (m_head != nullptr)
+            {
+                Node<T>* head_cpy = m_head;
+                m_head = m_head->next;
+                delete head_cpy;
+            }
+        }
+
+        // ===== UTILS =====
+        void print_in_order() const
+        {
+            
+        }
+        void print_reverse() const
+        {
+            
+        }
+    protected:
+        
+    private:
+        
+        // MEMBERS FUNCTIONS //
+    };
+    
+}
+
+
