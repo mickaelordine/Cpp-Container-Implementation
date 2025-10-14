697e9348b19025a8d78cffade3bbe824908dd89d
ordinemickael <ordinemickael99@gmail.com>
2025-10-14 18:30:41 +0200

    added list implementation

--- Container/List.h
+++ Container/List.h
@@ -1,8 +1,19 @@
 ﻿#include <iostream>
 #include <ostream>
 
+/*  
+ *  
+ *  
+ *  
+ */
+
+
+
 namespace MyStl
 {
+    template<typename T>
+    class List;
+    
     template<typename T>
     struct Node
     {
@@ -10,10 +21,16 @@ namespace MyStl
         Node<T>* next;
         Node<T>* prev;
 
-        Node() : data(), next(nullptr), prev(nullptr) {} // default constructor
+        Node() : data(), next(this), prev(this) {} // default constructor for sentinel node
         ~Node() = default;
         Node(const T& value) : data(value), next(nullptr), prev(nullptr) {} // copy constructor
-        Node(T&& value) : data(std::move(value)), next(nullptr), prev(nullptr) {} // Move constructor
+        Node(T&& value) noexcept : data(std::move(value)), next(nullptr), prev(nullptr) {} // Move constructor
+        
+    };
+
+    template<typename T>
+    class ListIterator
+    {
         
     };
     
@@ -27,28 +44,25 @@ namespace MyStl
         
     private:
         Node<T>* m_head;
-        Node<T>* m_tail;
         size_t m_size;
         // MEMBERS VARIABLES //
 
         // MEMBERS FUNCTIONS //
     public:
         // ===== CONSTRUCTORS =====
-        List() : m_head(nullptr), m_tail(nullptr), m_size(0){ std::cout << "List constructor called " << std::endl;} // default constructor
-        List(const List& other) : m_head(nullptr), m_tail(nullptr), m_size(0)
+        List() : m_head(new Node<T>()), m_size(0){ std::cout << "List constructor called " << std::endl;} // default constructor
+        List(const List& other) : m_head(new Node<T>()), m_size(0)
         {
-            Node<T>* temp = other.m_head;
-            while (temp != nullptr)
+            Node<T>* current = other.m_head->next;
+            while (current != other.m_head)
             {
-                // we copy all the element inside the new list
-                push_back(temp->data);
-                temp = temp->next;
+                push_back(current->data);
+                current = current->next;
             }
         } // copy constructor
-        List(const List&& other) noexcept : m_head(other.m_head), m_tail(other.m_tail), m_size(other.Size())
+        List(List&& other) noexcept : m_head(other.m_head), m_size(other.size())
         {
-            other.m_head = nullptr;
-            other.m_tail = nullptr;
+            other.m_head = new Node<T>();
             other.m_size = 0;
         } // move construtor
         List& operator=(const List& other)
@@ -56,12 +70,11 @@ namespace MyStl
             if (this != &other)
             {
                 clear();
-                Node<T>* temp = other.m_head;
-                while (temp != nullptr)
+                Node<T>* current = other.m_head->next;
+                while (current != other.m_head)
                 {
-                    // we copy all the element inside the new list
-                    push_back(temp->data);
-                    temp = temp->next;
+                    push_back(current->data);
+                    current = current->next;
                 }
             }
             return *this;
@@ -71,11 +84,12 @@ namespace MyStl
             if (this != &other)
             {
                 clear();
+                delete m_head;
+                
                 m_head = other.m_head;
-                m_tail = other.m_tail;
                 m_size = other.m_size;
-                other.m_head = nullptr;
-                other.m_tail = nullptr;
+                
+                other.m_head = new Node<T>();
                 other.m_size = 0;
             }
             return *this;
@@ -83,83 +97,218 @@ namespace MyStl
         ~List()
         {
             clear();
+            delete m_head;
             std::cout << "List destructor called" << std::endl;
         }
 
-        // ===== GETTER =====
-        inline Node<T>* Head(){ return m_head; }
-        inline Node<T>* Tail() { return m_tail; }
-
         // ===== CAPACITY =====
-        inline size_t Size() const { return m_size; }
-        inline bool Empty() const { return m_size == 0; }
+        inline size_t size() const { return m_size; }
+        inline bool empty() const { return m_size == 0; }
+
+        // ===== ITERATORS =====
+        ListIterator<T> begin() { return ListIterator<T> (); }
+        ListIterator<T> end() { return ListIterator<T> (); }
 
         // ===== ELEMENT ACCESS =====
+        
         T& front()
         {
-            
+            if (empty())
+                throw std::out_of_range("List::front - empty list");
+            return m_head->next->data;
         }
         const T& front() const
         {
-            
+            if (empty())
+                throw std::out_of_range("List::front - empty list");
+            return m_head->next->data;
         }
+        
         T& back()
         {
-            
+            if (empty())
+                throw std::out_of_range("List::back - empty list");
+            return m_head->prev->data;
         }
         const T& back() const
         {
-            
+            if (empty())
+                throw std::out_of_range("List::back - empty list");
+            return m_head->prev->data;
         }
 
         // ===== ELEMENT MODIFIERS =====
+        
         void push_back(const T& value)
         {
+            Node<T>* newNode = new Node<T>(value);
+            // we take the last node
+            Node<T>* lastNode = m_head->prev; 
             
-        }
-        void push_front(const T& value)
-        {
-            
+            newNode->next = m_head;
+            newNode->prev = lastNode;
+            lastNode->next = newNode;
+            m_head->prev = newNode;
+
+            ++m_size;
+            return;
         }
         void push_back(const T&& value)
         {
+            // normal case
+            Node<T>* newNode = new Node<T>(value);
+            // we take the last node
+            Node<T>* lastNode = m_head->prev; 
             
+            newNode->next = m_head;
+            newNode->prev = lastNode;
+            lastNode->next = newNode;
+            m_head->prev = newNode;
+
+            ++m_size;
+            return;
+        }
+        void push_front(const T& value)
+        {
+            Node<T>* newNode = new Node<T>(value);
+            // we take the last node
+            Node<T>* firstNode = m_head->next; 
+                
+            newNode->prev = m_head;
+            newNode->next = firstNode;
+            firstNode->prev = newNode;
+            m_head->next = newNode;
+    
+            ++m_size;
+            return;
         }
         void push_front(const T&& value)
         {
-            
+            Node<T>* newNode = new Node<T>(value);
+            // we take the last node
+            Node<T>* firstNode = m_head->next; 
+                
+            newNode->prev = m_head;
+            newNode->next = firstNode;
+            firstNode->prev = newNode;
+            m_head->next = newNode;
+    
+            ++m_size;
+            return;
         }
+        
         void pop_front()
         {
-            
+            if (empty()) 
+                throw std::out_of_range("Cannot pop_front from empty list");
+    
+            Node<T>* firstNode = m_head->next;
+    
+            // Collega m_head direttamente al secondo nodo
+            m_head->next = firstNode->next;
+            firstNode->next->prev = m_head;
+    
+            delete firstNode;
+            --m_size;
         }
         void pop_back()
         {
+            if (empty()) 
+                throw std::out_of_range("Cannot pop_back from empty list");
+    
+            Node<T>* firstNode = m_head->prev;
+    
+            // Collega m_head direttamente al penultimo nodo
+            m_head->prev = firstNode->prev;
+            firstNode->prev->next = m_head;
+    
+            delete firstNode;
+            --m_size;
+        }
+        
+        ListIterator<T> insert(ListIterator<T> it, const T& value)
+        {
+
+
+            return ListIterator<T> ();
+        }
+        ListIterator<T> erase(ListIterator<T> it)
+        {
+
+
+            return ListIterator<T> ();
+        }
+        ListIterator<T> erase(ListIterator<T> itBegin, ListIterator<T> itEnd)
+        {
+
+
+            return ListIterator<T> ();
+        }
+        
+        void remove(const T& value)
+        {
             
+        }
+        void reverse()
+        {
+            
+        }
+        void sort()
+        {
+            
+        }
+        void merge(List& other)
+        {
+            if (this != &other)
+            {
+                
+            }
         }
         void clear()
         {
-            while (m_head != nullptr)
+            while (!empty())
             {
-                Node<T>* head_cpy = m_head;
-                m_head = m_head->next;
-                delete head_cpy;
+                pop_front();
             }
         }
 
         // ===== UTILS =====
-        void print_in_order() const
+        void print() const
         {
-            
+            std::cout << "List [" << m_size << " elements]: ";
+            Node<T>* current = m_head->next;
+            std::cout << " [ Head ] -> ";
+            while (current != m_head)
+            {
+                std::cout << current->data;
+                if (current->next != m_head) 
+                    std::cout << " <-> ";
+                current = current->next;
+            }
+            std::cout << " <- [ Head ] " << std::endl;
         }
         void print_reverse() const
         {
-            
+            std::cout << "List (reverse) [" << m_size << " elements]: ";
+            Node<T>* current = m_head->prev;
+            std::cout << " [ Head ] -> ";
+            while (current != m_head)
+            {
+                std::cout << current->data;
+                if (current->prev != m_head) 
+                    std::cout << " <-> ";
+                current = current->prev;
+            }
+            std::cout << " <- [ Head ] ";
+            std::cout << std::endl;
         }
+        
     protected:
         
-    private:
         
+    private:
+        // ===== GETTER =====
+        inline Node<T>* head(){ return m_head; }
         // MEMBERS FUNCTIONS //
     };
     
--- Container/Main.cpp
+++ Container/Main.cpp
@@ -1,6 +1,221 @@
 
+#include <iostream>
+#include <list>
+#include <ostream>
+
+#include "List.h"
+
+namespace ListTest
+    {
+        // ===== FUNZIONI DI TEST =====
+        void test_basic_operations()
+        {
+            std::cout << "\n========== TEST: Basic Operations ==========\n";
+            MyStl::List<int> list;
+            
+            std::cout << "Empty list: " << (list.empty() ? "YES" : "NO") << std::endl;
+            std::cout << "Size: " << list.size() << std::endl;
+            
+            list.push_back(10);
+            list.push_back(20);
+            list.push_back(30);
+            list.print();
+            
+            list.push_front(5);
+            list.push_front(1);
+            list.print();
+            list.print_reverse();
+            
+            std::cout << "Front: " << list.front() << std::endl;
+            std::cout << "Back: " << list.back() << std::endl;
+            std::cout << "Size: " << list.size() << std::endl;
+            std::cout << "Empty: " << (list.empty() ? "YES" : "NO") << std::endl;
+        }
+
+        void test_pop_operations()
+        {
+            std::cout << "\n========== TEST: Pop Operations ==========\n";
+            MyStl::List<int> list;
+            
+            list.push_back(10);
+            list.push_back(20);
+            list.push_back(30);
+            list.push_back(40);
+            list.print();
+            
+            list.pop_front();
+            std::cout << "After pop_front: ";
+            list.print();
+            
+            list.pop_back();
+            std::cout << "After pop_back: ";
+            list.print();
+            
+            list.pop_front();
+            list.pop_back();
+            std::cout << "After popping all: ";
+            list.print();
+            std::cout << "Empty: " << (list.empty() ? "YES" : "NO") << std::endl;
+        }
+
+        void test_copy_constructor()
+        {
+            std::cout << "\n========== TEST: Copy Constructor ==========\n";
+            MyStl::List<int> list1;
+            list1.push_back(1);
+            list1.push_back(2);
+            list1.push_back(3);
+            
+            std::cout << "Original list: ";
+            list1.print();
+            
+            MyStl::List<int> list2 = list1;
+            std::cout << "Copied list: ";
+            list2.print();
+            
+            list1.push_back(999);
+            std::cout << "After modifying original: ";
+            list1.print();
+            std::cout << "Copy unchanged: ";
+            list2.print();
+        }
+
+        void test_move_constructor()
+        {
+            std::cout << "\n========== TEST: Move Constructor ==========\n";
+            MyStl::List<int> list1;
+            list1.push_back(10);
+            list1.push_back(20);
+            list1.push_back(30);
+            
+            std::cout << "Original list: ";
+            list1.print();
+            
+            MyStl::List<int> list2 = std::move(list1);
+            std::cout << "Moved list: ";
+            list2.print();
+            std::cout << "Original after move (should be empty): ";
+            list1.print();
+        }
+
+        void test_assignment_operators()
+        {
+            std::cout << "\n========== TEST: Assignment Operators ==========\n";
+            MyStl::List<int> list1;
+            list1.push_back(1);
+            list1.push_back(2);
+            
+            MyStl::List<int> list2;
+            list2.push_back(99);
+            
+            std::cout << "Before assignment:\n";
+            std::cout << "List1: ";
+            list1.print();
+            std::cout << "List2: ";
+            list2.print();
+            
+            list2 = list1;  // Copy assignment
+            std::cout << "\nAfter copy assignment:\n";
+            std::cout << "List1: ";
+            list1.print();
+            std::cout << "List2: ";
+            list2.print();
+            
+            MyStl::List<int> list3;
+            list3 = std::move(list1);  // Move assignment
+            std::cout << "\nAfter move assignment:\n";
+            std::cout << "List3: ";
+            list3.print();
+            std::cout << "List1 (should be empty): ";
+            list1.print();
+        }
+
+        void test_clear()
+        {
+            std::cout << "\n========== TEST: Clear ==========\n";
+            MyStl::List<int> list;
+            list.push_back(10);
+            list.push_back(20);
+            list.push_back(30);
+            
+            std::cout << "Before clear: ";
+            list.print();
+            
+            list.clear();
+            std::cout << "After clear: ";
+            list.print();
+            std::cout << "Size: " << list.size() << std::endl;
+            std::cout << "Empty: " << (list.empty() ? "YES" : "NO") << std::endl;
+            
+            // Test che possiamo ancora usare la lista dopo clear
+            list.push_back(100);
+            std::cout << "After adding element: ";
+            list.print();
+        }
+
+        void test_exceptions()
+        {
+            std::cout << "\n========== TEST: Exceptions ==========\n";
+            MyStl::List<int> list;
+            
+            try {
+                list.front();
+            } catch (const std::out_of_range& e) {
+                std::cout << "✓ Caught exception on empty front(): " << e.what() << std::endl;
+            }
+            
+            try {
+                list.back();
+            } catch (const std::out_of_range& e) {
+                std::cout << "✓ Caught exception on empty back(): " << e.what() << std::endl;
+            }
+            
+            try {
+                list.pop_front();
+            } catch (const std::out_of_range& e) {
+                std::cout << "✓ Caught exception on empty pop_front(): " << e.what() << std::endl;
+            }
+            
+            try {
+                list.pop_back();
+            } catch (const std::out_of_range& e) {
+                std::cout << "✓ Caught exception on empty pop_back(): " << e.what() << std::endl;
+            }
+        }
+
+        void test_with_strings()
+        {
+            std::cout << "\n========== TEST: With Strings ==========\n";
+            MyStl::List<std::string> list;
+            
+            list.push_back("Hello");
+            list.push_back("World");
+            list.push_front("C++");
+            list.push_back("!");
+            
+            list.print();
+            list.print_reverse();
+        }
+    }
+
 int main(int argc, char* argv[])
 {
+    ListTest::test_basic_operations();
+    std::cout << "\n===================\n";
+    ListTest::test_pop_operations();
+    std::cout << "\n===================\n";
+    ListTest::test_copy_constructor();
+    std::cout << "\n===================\n";
+    ListTest::test_move_constructor();
+    std::cout << "\n===================\n";
+    ListTest::test_assignment_operators();
+    std::cout << "\n===================\n";
+    ListTest::test_clear();
+    std::cout << "\n===================\n";
+    ListTest::test_exceptions();
+    std::cout << "\n===================\n";
+    ListTest::test_with_strings();
+    std::cout << "\n===================\n";
     
     return 0;
 }
